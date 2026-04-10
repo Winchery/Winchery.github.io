6030057aa97123a7481bd072b1898e0980e27d92
Winchery <3339694252@qq.com>
2026-04-10 19:36:09 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("网页加载成功！");
+function showResult() {
+  // 不管怎么选，结果都一样
+  document.getElementById("result").innerHTML = `
+    测试结果：<br><br>
+    你是一个<strong>温柔、认真、重感情、值得信赖</strong>的人。<br>
+    内心细腻，有责任感，对身边的人很照顾，
+    虽然偶尔会有点小纠结，但总体非常可靠，大家都很喜欢你～
+  `;
+}
